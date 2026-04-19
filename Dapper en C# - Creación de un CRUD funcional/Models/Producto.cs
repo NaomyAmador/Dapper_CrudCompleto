fc84d53930088b5e9b269e170ac7488f98109ba6
naomyamador_04 <naomyabigail2009@gmail.com>
2026-04-19 19:33:40 -0400

    Clase Producto creada en la carpeta Models

---
+++ Dapper en C# - Creación de un CRUD funcional/Models/Producto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dapper_en_C____Creación_de_un_CRUD_funcional.Models
+{
+    public class Producto
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public decimal Precio { get; set; }
+        public int Stock { get; set; }
+    }
+}
