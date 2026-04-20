37b15bd332b1b21d561600f5fa34a6b4bfb92f1a
naomyamador_04 <naomyabigail2009@gmail.com>
2026-04-19 22:32:46 -0400

    Cambios en los botones para Actualizar y Eliminar realizados

--- Dapper en C# - Creación de un CRUD funcional/Form1.cs
+++ Dapper en C# - Creación de un CRUD funcional/Form1.cs
@@ -60,13 +60,19 @@ namespace Dapper_en_C____Creación_de_un_CRUD_funcional
         {
             var Producto = new Producto
             {
-                //Aquí se le indica al DataGridView que seleccione en la tabla la fila que tenemos seleccionada mediante el Id
-                //y que este mismo nos muestre solo los datos pertenecientes al registro con ese Id, y que este nos lo devuelva en texto.
-                Id = int.Parse(DataGridView_TablaProductos.CurrentRow.Cells["Id"].Value.ToString()),
-
+                Id = int.Parse(TxtBox_Id.Text),
                 Nombre = TxtBox_Nombre.Text,
                 Precio = decimal.Parse(TxtBox_Precio.Text),
                 Stock = int.Parse(TxtBox_Stock.Text)
+
+
+                //Alternativa para no usat TxtBox_Id: Aquí se le indica al DataGridView que seleccione en la tabla la fila que tenemos seleccionada mediante el Id
+                //y que este mismo nos muestre solo los datos pertenecientes al registro con ese Id, y que este nos lo devuelva en texto.
+                //Id = int.Parse(DataGridView_TablaProductos.CurrentRow.Cells["Id"].Value.ToString()),
+
+                //Nombre = TxtBox_Nombre.Text,
+                //Precio = decimal.Parse(TxtBox_Precio.Text),
+                //Stock = int.Parse(TxtBox_Stock.Text)
             };
 
             RepositorioProducto.ActualizarProducto(Producto);
@@ -75,8 +81,11 @@ namespace Dapper_en_C____Creación_de_un_CRUD_funcional
 
         private void Btn_Eliminar_Click(object sender, EventArgs e)
         {
+            int id = int.Parse(TxtBox_Id.Text);
+
+            //Alternativa para no usat TxtBox_Id
             //Nota: Aquí se toma el Id del producto seleccionado en la tabla (como en el anterior).
-            int id = int.Parse(DataGridView_TablaProductos.CurrentRow.Cells["Id"].Value.ToString());
+            //int id = int.Parse(DataGridView_TablaProductos.CurrentRow.Cells["Id"].Value.ToString());
 
             RepositorioProducto.EliminarProducto(id);
             MessageBox.Show("Producto eliminado correctamente");
