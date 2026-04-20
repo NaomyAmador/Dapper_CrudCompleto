7208fc89767ed1360c16ae1cf3cbe390cc21b333
naomyamador_04 <naomyabigail2009@gmail.com>
2026-04-19 20:35:30 -0400

    Clase ProductoRepository creada en la carpeta Repositories, constructor y cadena de conexión hecha en esta

---
+++ Dapper en C# - Creación de un CRUD funcional/Repositories/ProductoRepository.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dapper;
+using System.Data.SqlClient;
+using Dapper_en_C____Creación_de_un_CRUD_funcional.Models;
+
+namespace Dapper_en_C____Creación_de_un_CRUD_funcional.Repository
+{
+    public class ProductoRepository
+    {
+        //readonly significa que el contenido de esa variable NO se podrá modificar.
+        private readonly string CadenaConexión;
+
+        //El constructor debe de tener el mismo nombre que la clase para que VS
+        //lo reconozca como tal y no como un método común.
+        public ProductoRepository(string CadenaConexión)
+        {
+            this.CadenaConexión = CadenaConexión;
+        }
+
+        //Método para crear una conexión a la base de datos
+        private SqlConnection CrearConexión()
+        {
+            return new SqlConnection(CadenaConexión);
+        }
+    }
+}
